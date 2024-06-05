aef812bfe029222e2b57675b054f0e6848ece5e4
gusry <gusry513@g.skku.edu>
2024-06-05 16:38:15 +0900

    input player num

--- main.c
+++ main.c
@@ -5,8 +5,33 @@
 #include <unistd.h>
 #include <time.h>
 
+typedef struct Player {
+	int isPlayer; 	// -1: death, 0:computer, 1:player
+	double SPRB;  // shooting probablity 0.01 ~ 1.0
+}Player;
+
+int input_num();
 
 void main(void){
-	
+	//variable
+	Player P[6]; //player arr 1/2/3/4/5
+
+	int player_num = input_num();
+
 	printf("test\n");
+}
+
+int input_num(){
+	int n;
+	fflush(stdin);
+	printf("Input Player numbers (Max:5) : ");
+	scanf(" %d", &n);
+	fflush(stdin);
+	printf("Player numbers is : %d\nGame start.\n\n",n);
+	if(n > 5 || n <= 0){
+		printf("Invalid input human player (1 ~ 5)\n");
+		printf("\n\n");
+		return input_num();
+	}	
+	return n;
 }
